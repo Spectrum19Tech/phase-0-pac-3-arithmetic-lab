f6eedb4aaa9d20850bd3684a4c24a56c3cd370c8
Jonathan Bryant <spectrum19tech@gmail.com>
2021-12-19 22:58:49 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    n = parseInt(n, 10);
+    return n;
+}
+
+function preserveDecimal(n) {
+    n = parseFloat(n);
+    return n;
+}
